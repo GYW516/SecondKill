8f8790db7c1253c6675e70a9a78daee618637d00
gyw <2917886013@qq.com>
2024-08-29 15:32:32 +0800

    源代码目录

--- src/main/java/com/gyw/SecondKillApplication.java
+++ src/main/java/com/gyw/SecondKillApplication.java
@@ -1,4 +1,4 @@
-package com.gyw.secondkill;
+package com.gyw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/gyw/SecondKillApplicationTests.java
+++ src/test/java/com/gyw/SecondKillApplicationTests.java
@@ -1,4 +1,4 @@
-package com.gyw.secondkill;
+package com.gyw;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
